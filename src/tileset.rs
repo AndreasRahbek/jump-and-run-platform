7a1b4c1542ed7f15e7fda57aa13965d0b5c6103d
Alexander Wentzel <alexwentzel@live.dk>
2025-03-18 12:04:10 +0100

    add background

--- src/tileset.rs
+++ src/tileset.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 pub fn setup_map(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Sprite {
-        image: asset_server.load("tileset/roads.png"),
+        image: asset_server.load("tileset/background.png"),
         image_mode: SpriteImageMode::Tiled {
             tile_x: true,
             tile_y: true,
-            stretch_value: 0.5, // The image will tile every 128px
+            stretch_value: 1.0,
+            
         },
         ..default()
     });
