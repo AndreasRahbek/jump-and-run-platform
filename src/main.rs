901d67f311baca1a01adaaf1ed6bd503a7cbaa78
Alexander Wentzel <alexwentzel@live.dk>
2025-03-18 11:08:11 +0100

    tileset

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod character_run;
+mod tileset;
 
 use bevy::prelude::*;
-use crate::character_run::{animate_sprite, setup};
+use character_run::{animate_sprite, setup};
+use tileset::Startup;{setupMap};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))// prevents blurry sprites
         .add_systems(Startup, setup)
         .add_systems(Update, animate_sprite)
+        .add_systems(Update, setupMap)
         .run();
 
 }
