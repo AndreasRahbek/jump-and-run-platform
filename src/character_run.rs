93fc550f9fbfa703df40007800cc1330579b6107
Andreas Rahbek Hansen <andreasrh7913@gmail.com>
2025-03-18 10:59:54 +0100

    restructure

---
+++ src/character_run.rs
@@ -0,0 +1,54 @@
+use bevy::asset::{AssetServer, Assets};
+use bevy::math::{UVec2, Vec3};
+use bevy::prelude::{Camera2d, Commands, Component, Deref, DerefMut, Query, Res, ResMut, Sprite, TextureAtlas, TextureAtlasLayout, Time, Timer, TimerMode, Transform};
+
+#[derive(Component)]
+pub struct AnimationIndices {
+    pub first: usize,
+    pub last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+pub struct AnimationTimer(pub Timer);
+
+pub fn animate_sprite(time: Res<Time>, mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut Sprite)>,
+) {
+    for(indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+
+        if timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                atlas.index = if atlas.index == indices.last {
+                    indices.first
+                } else {
+                    atlas.index + 1
+                }
+            }
+        }
+    }
+}
+
+pub fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("textures/chars/Biker/Biker_run.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(48), 6, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    let animation_indices = AnimationIndices {first: 0, last: 5};
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite::from_atlas_image(
+            texture,
+            TextureAtlas {
+                layout: texture_atlas_layout,
+                index: animation_indices.first,
+            },
+        ),
+        Transform::from_scale(Vec3::splat(6.0)),
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+mod character_run;
+
 use bevy::prelude::*;
+use crate::character_run::{animate_sprite, setup};
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))// prevents blurry sprites
@@ -8,56 +12,7 @@ fn main() {
 
 }
 
-#[derive(Component)]
-struct AnimationIndices {
-    first: usize,
-    last: usize,
-}
 
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-fn animate_sprite(time: Res<Time>, mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut Sprite)>,
-) {
-    for(indices, mut timer, mut sprite) in &mut query {
-        timer.tick(time.delta());
-
-        if timer.just_finished() {
-            if let Some(atlas) = &mut sprite.texture_atlas {
-                atlas.index = if atlas.index == indices.last {
-                    indices.first
-                } else {
-                    atlas.index + 1
-                }
-            }
-        }
-    }
-}
-
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let texture = asset_server.load("textures/chars/Biker/Biker_run.png");
-    let layout = TextureAtlasLayout::from_grid(UVec2::splat(48), 6, 1, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
-    let animation_indices = AnimationIndices {first: 0, last: 5};
-    commands.spawn(Camera2d);
-    commands.spawn((
-        Sprite::from_atlas_image(
-            texture,
-            TextureAtlas {
-                layout: texture_atlas_layout,
-                index: animation_indices.first,
-            },
-        ),
-        Transform::from_scale(Vec3::splat(6.0)),
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-    ));
-}
 
 
 
